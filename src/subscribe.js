898b5f7f2bbaa238121e8e6ce8f90758ae7c1d74
andreatugnoli <andrea.tugnoli@radicalbit.io>
2021-12-10 16:35:02 +0100

    use new field names standard

--- src/subscribe.js
+++ src/subscribe.js
@@ -1,12 +1,13 @@
 const { HeliconSubscribeClient } = require("@radicalbit/helicon-node-sdk");
 
-const grpcHost = "<grpc-host>";
-const grpcPort = 0;
+const host = "<host>";
+const port = 443;
 const clientId = "<client-id>";
 const clientSecret = "<client-secret>";
 const tenant = "<tenant-name>";
+const protocol = "<http-protocol>";
 
-const heliconSubscribeClient = new HeliconSubscribeClient(grpcHost, grpcPort, clientId, clientSecret, tenant);
+const heliconSubscribeClient = new HeliconSubscribeClient(host, clientId, clientSecret, tenant, port, protocol);
 
 heliconSubscribeClient.subscribe("<streamName>").onData(
   (data) => console.log(data)
